e5d29692a317627df9ec1871583ad01985c00c28
WenDEVLIFE <frouenmedinajr@gmail.com>
2024-06-20 13:31:39 +0800

    Added box and opacity to the image 👾

--- app/src/main/java/com/example/happybirthdaycompose/MainActivity.kt
+++ app/src/main/java/com/example/happybirthdaycompose/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -15,6 +17,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,11 +35,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(
-                        message = "Happy Birthday Sam!",
-                        from = "From Emma",
-                        modifier = Modifier.padding(8.dp)
-                    )
+                    GreetingImage(message = "Happy Birthday Jul" , from = "Wen2x")
                 }
             }
         }
@@ -64,10 +64,33 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
     }
 }
 
-@Preview(showBackground = true)
+
+
 @Composable
-fun BirthdayCardPreview() {
-    HappyBirthdayComposeTheme {
-        GreetingText(message = "Happy Birthday Sam!", from = "From Emma")
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.petalsbg)
+    Box(modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5f,
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
     }
 }
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    HappyBirthdayComposeTheme {
+        GreetingImage(message = "Happy Birthday Jul" , from = "Wen2x")
+    }
+}
+
